a1b19b52d9fec0361a29db62ed7a0e5713914da4
Markus Mertama <mertama@gmail.com>
2023-07-21 22:57:57 +0300

    1st version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pen
-vector draw command line
+
+Draw lines from command line
+
+Requires Gempyre-Python to be installed
+
+* off x y - does offset to parameters
+
+* scale s - scales drawing
+
+* ln x1 y1 x2 y2 - draw a line
+
+e.g.
+
+```bash
+
+$ ./pen.py off 1000 0 ln 1322 12 1350 100
+
+```
---
+++ pen.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import Gempyre
+import os
+import sys
+from Gempyre_utils import resource
+
+
+def command(ui, args):
+    canvas = Gempyre.CanvasElement(ui, "canvas")
+
+    fc = Gempyre.FrameComposer()
+    it = iter(args)
+
+    offy = 0.
+    offx = 0.
+    scale = 1.
+
+
+    def posx(x):
+        return (float(x) - offx) * scale
+
+    def posy(y):
+        return (float(y) - offy) * scale    
+
+    try:
+        while it:
+            cmd = next(it) 
+            if(cmd == 'off'):
+                offx = float(next(it))
+                offy = float(next(it))
+            elif(cmd == 'scale'):
+                scale = float(next(it))    
+            elif(cmd == 'ln'):
+                fc.begin_path()
+                fc.move_to(posx(next(it)), posy(next(it)))
+                fc.line_to(posx(next(it)), posy(next(it)))
+                fc.close_path()
+                fc.stroke()
+            else:
+                print("Not get: ", cmd, file=std.err)
+    except (StopIteration):
+        pass
+    
+    canvas.draw_frame(fc)    
+
+
+if __name__ == "__main__":
+    name = os.path.join(os.path.dirname(sys.argv[0]), "ui", "ui.html")
+    map, names = resource.from_file(name)
+    ui = Gempyre.Ui(map, names[name])
+
+    ui.on_open(lambda: command(ui, sys.argv[1:]))
+
+    ui.run()
